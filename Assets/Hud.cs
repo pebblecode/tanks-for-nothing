8c5b73c3dc42e9606c82818341f3bf620d9573df
David Hillier <daveh@Davids-MacBook-Pro.local>
2016-03-08 17:00:58 +0000

    Added scores

--- Assets/Hud.cs
+++ Assets/Hud.cs
@@ -10,26 +10,42 @@ public class Hud : MonoBehaviour
 
 	public GameObject[] players;
 
+	public UnityEngine.UI.Text[] scores;
 	// Use this for initialization
 	void Start ()
 	{
-		score = GetComponent<UnityEngine.UI.Text> ();
-		score.text = "WOAH";
+		//score = GetComponent<UnityEngine.UI.Text> ();
+		//score.text = "WOAH";
 
-
+		scores = GetComponentsInChildren<UnityEngine.UI.Text> ();
+		foreach (var s in scores) {
+			s.gameObject.SetActive (false);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		players = GameObject.FindGameObjectsWithTag ("Player");
+		foreach (var player in players) {
+			var playerController = player.GetComponent<PlayerController> ();
+			var health = player.GetComponent<TankHealth> ();
+			var pi = playerController.playerNumber - 1;
+			scores[pi].text = string.Format("Health: {0}\nAmmo: {1}\n", 
+				Mathf.Ceil(health.health), health.ammo);
+			scores [pi].color = playerController.tankColour;
+			scores [pi].gameObject.SetActive (true);
+		}
+
+		/*players = GameObject.FindGameObjectsWithTag ("Player");
 
 		var sb = new System.Text.StringBuilder ();
 		foreach (var player in players) {
 			var playerController = player.GetComponent<PlayerController> ();
 			var health = player.GetComponent<TankHealth> ();
-			sb.AppendFormat("{0}: Health {1} Ammo {2}\n", playerController.playerNumber, Mathf.Ceil(health.health), health.ammo);
+			if (playerController != null && health != null)
+				sb.AppendFormat("{0}: Health {1} Ammo {2}\n", playerController.playerNumber, Mathf.Ceil(health.health), health.ammo);
 		}
-		score.text = sb.ToString ();
+		score.text = sb.ToString ();*/
 	}
 }
