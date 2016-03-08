837ef4938fbf2c2513335b9b9187b2475f9070cf
Jonathan King <jonathan.j.king@gmail.com>
2016-03-08 16:47:23 +0000

    Fonts and menu

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Timers;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+  Timer tmr = new Timer(1000);
+
+  // Use this for initialization
+  void Start () {
+    // tmr.Interval = 1000; // 0.1 second
+    // tmr.Elapsed += timerHandler; // We'll write it in a bit
+    // tmr.Start(); // The countdown is launched!
+  }
+
+  // private void timerHandler (object sender, ElapsedEventArgs e) {
+  //   var thisText = GetComponent<Text> ();
+  //   thisText.text = "Tanks";
+  //   tmr.Stop();
+  // }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 }
+  
