5157cece08d7dfd2100c300761e90fc22a69d73e
Mike James <m.l.james@me.com>
2016-03-08 16:41:41 +0000

    pew!

--- Assets/Audio.cs
+++ Assets/Audio.cs
@@ -3,19 +3,27 @@ using System.Collections;
 
 public class Audio : MonoBehaviour {
 	public AudioSource fireAudioSource;
-	public AudioClip fire;
+	public AudioClip [] fire;
 	public AudioSource engineAudioSource;
 	public AudioClip idle;
 	public AudioClip driving;
 	private float lastShot;
+	private PlayerController _player;
+	private PlayerController player { 
+		get {
+			if (_player == null) {
+				_player = GetComponent<PlayerController> ();
+			}
+			return _player;
+		}
+	}
 
 	public void Fire() {
-		fireAudioSource.clip = fire;
+		fireAudioSource.clip = fire[player.playerNumber];
 		fireAudioSource.Play();		
 	}
 
 	public void Update () {
-		var player = GetComponent<PlayerController> ();
 
 		if (player.leftStick.magnitude > 0 && engineAudioSource.clip != driving) { 
 			engineAudioSource.clip = driving;
@@ -26,7 +34,5 @@ public class Audio : MonoBehaviour {
 			engineAudioSource.clip = idle;
 			engineAudioSource.Play ();
 		}
-
-
 	}
 }
